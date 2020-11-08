ee64f4c389cc8258829627d03802069db9a8d7c8
1hbb <1fobofobi@gmail.com>
2020-11-08 18:15:15 +0300

    Update

--- src/Panel.java
+++ src/Panel.java
@@ -1,43 +1,50 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
+import java.util.ArrayList;
 import java.util.Random;
+import java.util.Scanner;
 
-public class Panel extends JPanel implements ActionListener {
+public class Panel extends JPanel {
 
-    static final int SCREEN_WIDTH = 600;
-    static final int SCREEN_HEIGHT = 600;
-    static final int UNIT_SIZE = 25;
-    static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT) / UNIT_SIZE;
-    static final int DELAY = 75;
-    final int x[] = new int[GAME_UNITS];
-    final int y[] = new int[GAME_UNITS];
-    int bodyParts = 6;
-    int applesEaten;
-    int appleX;
-    int appleY;
-    char direction = 'R';
+    static final int SCREEN_WIDTH = 800;
+    static final int SCREEN_HEIGHT = 800;
+    int UNIT_SIZE;
+    int coinCount;
+    ArrayList<Integer> coinCountXY = new ArrayList<Integer>();
+    ArrayList<Integer> coinX = new ArrayList<Integer>();
+    ArrayList<Integer> coinY = new ArrayList<Integer>();
     boolean running = false;
     Random random;
-    Timer timer;
-
+    int size = 0;
 
     Panel() {
+        Scanner sc = new Scanner(System.in);
+        String str1 = JOptionPane.showInputDialog("Boyutu Girin: ");
+        int size = Integer.parseInt(str1);
+        UNIT_SIZE = 800 / size;
+        coinCount = (SCREEN_HEIGHT / UNIT_SIZE * SCREEN_HEIGHT / UNIT_SIZE) * 20 / 100;
         random = new Random();
         this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
         this.setBackground(Color.WHITE);
         this.setFocusable(true);
-        this.addKeyListener(new MyKeyAdapter());
         startGame();
+        randomCoins();
 
+    }
+
+    public void randomCoins() {
+
+        for (int i = 0; i < coinCount; i++) {
+            coinX.add(random.nextInt((int) (SCREEN_WIDTH / UNIT_SIZE)) * UNIT_SIZE);
+            coinY.add(random.nextInt((int) (SCREEN_WIDTH / UNIT_SIZE)) * UNIT_SIZE);
+            coinCountXY.add((random.nextInt((int) (4)) + 1) * 5);
+        }
     }
 
     // for start the game
     public void startGame() {
-        newApple();
         running = true;
-        timer = new Timer(DELAY, this);
-        timer.start();
+
     }
 
     // for paint
@@ -52,45 +59,29 @@ public class Panel extends JPanel implements ActionListener {
             g.drawLine(0, i * UNIT_SIZE, SCREEN_WIDTH, i * UNIT_SIZE);
             g.setColor(Color.BLACK);
         }
-        g.setColor(Color.BLUE);
-        g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+        int counter = 0;
 
+        for (int i = 0; i < coinCount; i++) {
 
-    }
-
-    public void newApple() {
-        appleX = random.nextInt((int) (SCREEN_WIDTH / UNIT_SIZE)) * UNIT_SIZE;
-        appleY = random.nextInt((int) (SCREEN_HEIGHT / UNIT_SIZE)) * UNIT_SIZE;
+            if (i < Math.round(((coinCount) * 1 / 10))) {
+                g.setColor(Color.RED);
+                //g.fillRect(coinX[i], coinY[i], UNIT_SIZE, UNIT_SIZE);
+                //System.out.print(coinX[i] + " " + coinY[i] + "\n");
+                System.out.println("Gizli" + (coinCount) * 1 / 10);
+                g.drawString(String.valueOf(coinCountXY.get(i)), coinX.get(i), coinY.get(i));
+                System.out.println(coinX.get(i) + " " + coinY.get(i));
+            } else {
+                g.setColor(Color.BLUE);
+                g.drawString(String.valueOf(coinCountXY.get(i)), coinX.get(i), coinY.get(i));
+                System.out.println(coinX.get(i) + " " + coinY.get(i));
+            }
+        }
     }
 
     public void move() {
-        for (int i = bodyParts; i > 0; i++) {
 
-        }
 
     }
 
-    public void checkApple() {
 
-    }
-
-    public void checkCollisions() {
-
-    }
-
-    public void gameOver(Graphics g) {
-
-    }
-
-    public class MyKeyAdapter extends KeyAdapter {
-        @Override
-        public void keyPressed(KeyEvent e) {
-
-        }
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-
-    }
 }
