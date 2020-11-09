7f936cc8e5e0ba44290f2cdb13ad06743b2f0700
1hbb <1fobofobi@gmail.com>
2020-11-10 02:45:30 +0300

    Add coins to matrix

--- src/Panel.java
+++ src/Panel.java
@@ -13,12 +13,13 @@ public class Panel extends JPanel implements ActionListener {
     static final int SCREEN_HEIGHT = 800;
     int UNIT_SIZE;
     int coinCount;
-    ArrayList<Integer> coinCountXY = new ArrayList<Integer>();
-    ArrayList<Integer> coinX = new ArrayList<Integer>();
-    ArrayList<Integer> coinY = new ArrayList<Integer>();
+    ArrayList<Integer> coinCountXY = new ArrayList<>();
+    ArrayList<Integer> coinX = new ArrayList<>();
+    ArrayList<Integer> coinY = new ArrayList<>();
     boolean running = false;
     Random random;
-    int size;
+    int SIZE;
+    int[][] COIN_MATRIX = new int[SIZE][SIZE];
 
     int A_PLAYER_LOCATION_X;
     int A_PLAYER_LOCATION_Y;
@@ -33,12 +34,13 @@ public class Panel extends JPanel implements ActionListener {
     Panel() {
         String str1 = JOptionPane.showInputDialog("Boyutu Girin: ");
         int size = Integer.parseInt(str1);
+        SIZE = size;
         UNIT_SIZE = 800 / size;
         coinCount = (SCREEN_HEIGHT / UNIT_SIZE * SCREEN_HEIGHT / UNIT_SIZE) * 20 / 100;
         A_PLAYER_LOCATION_X = 0;
         A_PLAYER_LOCATION_Y = 0;
-        B_PLAYER_LOCATION_Y = 0;
         B_PLAYER_LOCATION_X = size - 1;
+        B_PLAYER_LOCATION_Y = 0;
         C_PLAYER_LOCATION_X = 0;
         C_PLAYER_LOCATION_Y = size - 1;
         D_PLAYER_LOCATION_X = size - 1;
@@ -56,35 +58,41 @@ public class Panel extends JPanel implements ActionListener {
     }
 
     public void randomCoins() {
-
-//        for (int i = 0; i < coinCount; i++) {
-//            coinX.add(i, random.nextInt((int) (SCREEN_WIDTH / UNIT_SIZE)) * UNIT_SIZE);
-//            coinY.add(i, random.nextInt((int) (SCREEN_WIDTH / UNIT_SIZE)) * UNIT_SIZE);
-//            coinCountXY.add(i, (random.nextInt((int) (4)) + 1) * 5);
-//        }
-        int eklenenAltın = 0;
-        int randX = 0;
-        int randY = 0;
-        int sayac = 0;
+        int randX;
+        int randY;
+        int counter;
         for (int i = 0; i < coinCount; i++) {
-            sayac = 0;
+            counter = 0;
             randX = random.nextInt((int) (SCREEN_WIDTH / UNIT_SIZE)) * UNIT_SIZE;
             randY = random.nextInt((int) (SCREEN_WIDTH / UNIT_SIZE)) * UNIT_SIZE;
             for (int j = 0; j < i; j++) {
                 if (randX == coinX.get(j) && randY == coinY.get(j)) {
-                    sayac--;
+                    counter--;
                 }
+
             }
-            if (sayac <= -1) {
+            if (counter <= -1) {
                 i--;
             } else {
                 coinX.add(i, randX);
                 coinY.add(i, randY);
-                eklenenAltın++;
                 coinCountXY.add(i, (random.nextInt((int) (4)) + 1) * 5);
             }
         }
-        System.out.println("altın sayisi" + eklenenAltın);
+
+        int[][] coinMatrix = new int[SIZE][SIZE];
+
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                for (int k = 0; k < coinCount; k++) {
+                    if ((coinX.get(k) / UNIT_SIZE) == i && (coinY.get(k) / UNIT_SIZE) == j) {
+                        coinMatrix[i][j] = coinCountXY.get(k);
+                    }
+                }
+            }
+        }
+
+        COIN_MATRIX = coinMatrix;
     }
 
     // for start the game
@@ -106,29 +114,29 @@ public class Panel extends JPanel implements ActionListener {
         g.drawString("C", C_PLAYER_LOCATION_X * UNIT_SIZE + UNIT_SIZE / 2, C_PLAYER_LOCATION_Y * UNIT_SIZE + UNIT_SIZE / 2);
         g.drawString("D", D_PLAYER_LOCATION_X * UNIT_SIZE + UNIT_SIZE / 2, D_PLAYER_LOCATION_Y * UNIT_SIZE + UNIT_SIZE / 2);
 
-        for (int i = 0; i < (SCREEN_HEIGHT / UNIT_SIZE); i++) {
+        for (int i = 0; i < SIZE; i++) {
             g.setColor(Color.BLACK);
             g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGHT);
             g.drawLine(0, i * UNIT_SIZE, SCREEN_WIDTH, i * UNIT_SIZE);
 
         }
-        int counter = 0;
-        for (int i = 0; i < coinCount; i++) {
 
-//            if (i < Math.round(((coinCount) * 1 / 10))) {
-//                g.setColor(Color.RED);
-//                System.out.println("Gizli" + (coinCount) * 1 / 10);
-//                g.drawString(String.valueOf(coinCountXY.get(i)), coinX.get(i), coinY.get(i));
-//                System.out.println(coinX.get(i) + " " + coinY.get(i));
-//            } else {
-            g.setColor(Color.BLUE);
-            g.drawString(String.valueOf(coinCountXY.get(i)), coinX.get(i) + UNIT_SIZE / 2, coinY.get(i) + UNIT_SIZE / 2);
-            //System.out.println("Size" + coinCountXY.size());
-            System.out.println(coinX.get(i) + " " + coinY.get(i));
-            counter++;
-            //}
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (COIN_MATRIX[i][j] != 0) {
+                    g.setColor(Color.BLUE);
+                    g.drawString(String.valueOf(COIN_MATRIX[i][j]), (j * UNIT_SIZE) + (UNIT_SIZE / 2), (i * UNIT_SIZE) + (UNIT_SIZE / 2));
+                }
+            }
         }
-        System.out.println(counter);
+
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                System.out.print(COIN_MATRIX[i][j] + " ");
+            }
+            System.out.println();
+        }
+
     }
 
     @Override
@@ -149,6 +157,22 @@ public class Panel extends JPanel implements ActionListener {
 
             }
         }
+    }
+
+    public void PLAYER_A() {
+
+    }
+
+    public void PLAYER_B() {
+
+    }
+
+    public void PLAYER_C() {
+
+    }
+
+    public void PLAYER_D() {
+
     }
 
     public void move() {
